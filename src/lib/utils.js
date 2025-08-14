2b1674d090a315ef8e5699a6fb1d4eb3384e58a0
Sonam-24 <sagar.sonam24@gmail.com>
2025-08-14 15:12:30 +0530

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,6 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
-
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
